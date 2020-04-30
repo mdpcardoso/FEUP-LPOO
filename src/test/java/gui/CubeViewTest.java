a17d8560de51add1b5dffb3a8dd5c36a64c8c151
Mario Cardoso <36649817+mdpcardoso@users.noreply.github.com>
2020-04-30 13:34:15 +0100

    Add CubeView test

---
+++ src/test/java/gui/CubeViewTest.java
@@ -0,0 +1,28 @@
+package gui;
+
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import data.CubeModel;
+import data.PlayerModel;
+import data.Position;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class CubeViewTest {
+
+    @Test
+    public void draw() {
+        TerminalScreen screen = Mockito.mock(TerminalScreen.class);
+        CubeModel cubeModel = Mockito.mock(CubeModel.class);
+
+        Mockito.when(cubeModel.getPosition()).thenReturn(new Position(1, 2));
+
+        CubeView cubeView = new CubeView();
+        cubeView.draw(screen, cubeModel);
+
+        Mockito.verify(screen, Mockito.times(1))
+                .setCharacter(1, 2, new TextCharacter((char)0x25a1));
+    }
+}
