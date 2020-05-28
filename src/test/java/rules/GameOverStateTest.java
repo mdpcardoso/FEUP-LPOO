0871798652a69d3c03d73640ba1e70fdccda238b
Mario Cardoso <36649817+mdpcardoso@users.noreply.github.com>
2020-05-28 16:56:50 +0100

    Test GamePauseState. Add tests to GameOverState.

--- src/test/java/rules/GameOverStateTest.java
+++ src/test/java/rules/GameOverStateTest.java
@@ -2,6 +2,7 @@ package rules;
 
 import gui.ArenaView;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
@@ -15,6 +16,20 @@ public class GameOverStateTest {
         GameOverState gameOverState = new GameOverState();
         gameOverState.execute(ArenaView.COMMAND.ACCEPT, controller);
 
+        Mockito.verify(controller.getGui()).drawGameOver(controller.getArena());
         Mockito.verify(controller.getArena().getOverlayModel()).setScore(0);
+        Mockito.verify(controller).setCurrentState(ArgumentMatchers.any(GameState.class));
+    }
+
+    @Test
+    public void executeNoAccept() {
+        // TODO: Check for an alternative
+        ArenaController controller = Mockito.mock(ArenaController.class, RETURNS_DEEP_STUBS);
+        GameOverState gameOverState = new GameOverState();
+        gameOverState.execute(ArenaView.COMMAND.EOF, controller);
+
+        Mockito.verify(controller.getGui()).drawGameOver(controller.getArena());
+        Mockito.verify(controller.getArena().getOverlayModel(), Mockito.never()).setScore(0);
+        Mockito.verify(controller, Mockito.never()).setCurrentState(ArgumentMatchers.any(GameState.class));
     }
 }
