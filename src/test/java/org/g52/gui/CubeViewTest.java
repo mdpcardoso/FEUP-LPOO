6747d5056baa315a373fc723323368c97371ea02
Mario Cardoso <36649817+mdpcardoso@users.noreply.github.com>
2020-05-29 12:26:17 +0100

    Test CubeView with PowerCube

--- src/test/java/org/g52/gui/CubeViewTest.java
+++ src/test/java/org/g52/gui/CubeViewTest.java
@@ -1,9 +1,12 @@
 package org.g52.gui;
 
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import org.g52.data.NormalCube;
 import org.g52.data.Position;
+import org.g52.data.PowerCube;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -20,6 +23,21 @@ public class CubeViewTest {
         cubeView.draw(screen, cube);
 
         Mockito.verify(screen, Mockito.times(1))
-                .setCharacter(1, 2, new TextCharacter((char)0x25a1));
+                .setCharacter(1, 2, new TextCharacter((char) 0x25a1));
+    }
+
+    @Test
+    public void drawPowerCube() {
+        TerminalScreen screen = Mockito.mock(TerminalScreen.class);
+        TextGraphics textGraphics = Mockito.mock(TextGraphics.class);
+        Mockito.when(screen.newTextGraphics()).thenReturn(textGraphics);
+
+        PowerCube powerCube = Mockito.mock(PowerCube.class);
+        Mockito.when(powerCube.getPosition()).thenReturn(new Position(1, 2));
+
+        CubeView cubeView = new CubeView();
+        cubeView.draw(screen, powerCube);
+        Mockito.verify(textGraphics).setForegroundColor(TextColor.ANSI.GREEN);
+        Mockito.verify(textGraphics).putString(1, 2, String.valueOf((char) 0x25a0));
     }
 }
