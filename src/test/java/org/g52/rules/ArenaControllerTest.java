13a937b59ae53ccc9abf253577d9f2339f7c0bd0
Mario Cardoso <36649817+mdpcardoso@users.noreply.github.com>
2020-05-29 21:00:17 +0100

    Improve ArenaControllerTest

--- src/test/java/org/g52/rules/ArenaControllerTest.java
+++ src/test/java/org/g52/rules/ArenaControllerTest.java
@@ -1,35 +1,40 @@
 package org.g52.rules;
 
 import org.g52.data.ArenaModel;
+import org.g52.data.CubeModel;
+import org.g52.data.OverlayModel;
+import org.g52.data.PlayerModel;
 import org.g52.gui.ArenaView;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
 
 public class ArenaControllerTest {
+    private  ArenaController arenaController;
+    private ArenaView gui;
+
+    @Before
+    public void setup() {
+        PlayerModel playerModel = new PlayerModel(5, 5);
+        ArenaModel arenaModel = new ArenaModel(10, 10, playerModel, new CubeModel(), new OverlayModel());
+        ControllerFactory factory = new ControllerFactory();
+        gui = Mockito.mock(ArenaView.class);
+
+        arenaController = new ArenaController(gui, arenaModel, factory);
+
+    }
 
     @Test
     public void executeCommandEOF() throws IOException {
-        ArenaModel arenaModel = Mockito.mock(ArenaModel.class);
-        ControllerFactory factory = Mockito.mock(ControllerFactory.class);
-        ArenaView gui = Mockito.mock(ArenaView.class);
-
-        ArenaController arenaController = new ArenaController(gui, arenaModel, factory);
-
         arenaController.executeCommand(ArenaView.COMMAND.EOF);
         Mockito.verify(gui).closeScreen();
     }
 
     @Test
     public void executeCommandNoEOF() throws IOException {
-        ArenaModel arenaModel = Mockito.mock(ArenaModel.class);
-        ControllerFactory factory = Mockito.mock(ControllerFactory.class);
-        ArenaView gui = Mockito.mock(ArenaView.class);
-
-        ArenaController arenaController = new ArenaController(gui, arenaModel, factory);
-
         arenaController.executeCommand(ArenaView.COMMAND.ACCEPT);
-        Mockito.verify(gui, Mockito.times(0)).closeScreen();
+        Mockito.verify(gui, Mockito.never()).closeScreen();
     }
 }
