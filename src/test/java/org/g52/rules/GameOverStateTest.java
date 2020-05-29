b1bbe6a427d3fffa504a3a1e4428736d410ce9cd
Mario Cardoso <36649817+mdpcardoso@users.noreply.github.com>
2020-05-29 21:33:47 +0100

    Improve GameOverStateTest

--- src/test/java/org/g52/rules/GameOverStateTest.java
+++ src/test/java/org/g52/rules/GameOverStateTest.java
@@ -1,35 +1,46 @@
 package org.g52.rules;
 
+import org.g52.data.*;
 import org.g52.gui.ArenaView;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.junit.Assert.assertEquals;
 
 public class GameOverStateTest {
+    private OverlayModel overlayModel;
+    private ArenaController arenaController;
+    private GameOverState gameOverState;
+
+    @Before
+    public void setup() {
+        PlayerModel playerModel = new PlayerModel(5, 5);
+        CubeModel cubeModel = new CubeModel();
+        cubeModel.addCube(new NormalCube(new Position(1, 2), 1));
+        overlayModel = new OverlayModel();
+        ArenaModel arenaModel = new ArenaModel(10, 10, playerModel, cubeModel, overlayModel);
+
+        ControllerFactory controllerFactory = new ControllerFactory();
+        arenaController = new ArenaController(Mockito.mock(ArenaView.class), arenaModel, controllerFactory);
+
+        gameOverState = new GameOverState();
+    }
 
     @Test
     public void execute() {
-        // TODO: Check for an alternative
-        ArenaController controller = Mockito.mock(ArenaController.class, RETURNS_DEEP_STUBS);
-        GameOverState gameOverState = new GameOverState();
-        gameOverState.execute(ArenaView.COMMAND.ACCEPT, controller);
+        overlayModel.setScore(5);
+        gameOverState.execute(ArenaView.COMMAND.ACCEPT, arenaController);
 
-        Mockito.verify(controller.getGui()).drawGameOver(controller.getArena());
-        Mockito.verify(controller.getArena().getOverlayModel()).setScore(0);
-        Mockito.verify(controller).setCurrentState(ArgumentMatchers.any(GameState.class));
+        assertEquals(0, overlayModel.getScore());
+        assertEquals(GameState.class, arenaController.getCurrentState().getClass());
     }
 
     @Test
     public void executeNoAccept() {
-        // TODO: Check for an alternative
-        ArenaController controller = Mockito.mock(ArenaController.class, RETURNS_DEEP_STUBS);
-        GameOverState gameOverState = new GameOverState();
-        gameOverState.execute(ArenaView.COMMAND.EOF, controller);
+        gameOverState.execute(ArenaView.COMMAND.EOF, arenaController);
 
-        Mockito.verify(controller.getGui()).drawGameOver(controller.getArena());
-        Mockito.verify(controller.getArena().getOverlayModel(), Mockito.never()).setScore(0);
-        Mockito.verify(controller, Mockito.never()).setCurrentState(ArgumentMatchers.any(GameState.class));
+        assertEquals(0, overlayModel.getScore());
+        assertEquals(GameStartState.class, arenaController.getCurrentState().getClass());
     }
 }
